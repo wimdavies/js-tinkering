15a3911ba510c55754116ea772da276db352d179
wimdavies <william.johnston.davies@gmail.com>
2023-05-23 11:06:20 +0100

    completed step 8 of Phase 1

---
+++ counter.js
@@ -0,0 +1,9 @@
+let counter = 0
+
+const increment = () => {
+  counter++
+
+  console.log(counter);
+}
+
+setInterval(increment, 1000);
---
+++ timeoutTrial.js
@@ -0,0 +1,9 @@
+console.log('1. The program starts');
+
+setTimeout(() => {
+  console.log('4. The timer is lapsed, this callback function is now called');
+}, 2000); // 2000ms = 2 seconds
+
+console.log('2. The timer has started, and the callback function registered to be called later');
+
+console.log('3. The rest of the program below (immediate tasks) will keep running immediately');
